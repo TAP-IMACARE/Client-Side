ff697e3e765cf6f23ff1dd9284e6a686dc73f203
CALEB <owatahcaleb@gmail.com>
2023-07-15 16:37:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-// import Footer from "./components/Footer/Footer";
-// import Header from "./components/Header/Header";
-// import Hero from "./components/Hero/Hero";
-// import OurDoctors from "./components/OurDoctors/OurDoctors";
-// import OurServices from "./components/OurServices/OurServices";
-// import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
-
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Homepage />
-      {/* <Header />
-      <Hero />
-      <WhyChooseUs />
-      <OurServices />
-      <OurDoctors />
-      <Footer /> */}
     </div>
   );
 }
