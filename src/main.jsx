fd545eebc719bb8aad42874088f9137d7cf3db5b
Dumebi Joy <ekwubiakunuba@gmail.com>
2023-07-17 11:18:13 +0100

    completed login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ScheduleAppointment from "./pages/ScheduleAppointment/ScheduleAppointment.jsx";
+import Login from "./pages/Login/Login.jsx"
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "schedule-appointment",
     element: <ScheduleAppointment />,
+  },
+  {
+    path: "login",
+    element: <Login />,
   },
 ]);
 
