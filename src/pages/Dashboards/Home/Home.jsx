6316b97574861b2d0f230a9656f7b2bfef8eabc5
CALEB <owatahcaleb@gmail.com>
2023-08-21 15:07:20 +0100

    Update Home.jsx

--- src/pages/Dashboards/Home/Home.jsx
+++ src/pages/Dashboards/Home/Home.jsx
@@ -1,21 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../../components/DashboardNav/Navbar";
 import Sidebar from "../../../components/Sidebar/Sidebar";
-import Overview from "../../../dashboards/Overview/Overview";
 import "./Home.css";
-
-// import addIcon from "../../../assets/dashboard assets/add.svg";
-// import requestIcon from "../../../assets/dashboard assets/RequestHome-icon.svg";
-// import rightArrow from "../../../assets/dashboard assets/rightArrow-icon.svg";
-// import leftArrow from "../../../assets/dashboard assets/leftArrow-icon.svg";
-// import avatar1 from "../../../assets/dashboard assets/Alice.svg";
-// import avatar2 from "../../../assets/dashboard assets/Bisola.svg";
-// import avatar3 from "../../../assets/dashboard assets/Ubong.svg";
-// import pdf from "../../../assets/dashboard assets/pdf-icon.svg";
-// import download from "../../../assets/dashboard assets/download-icon.svg";
-// import whiteArrow from "../../../assets/dashboard assets/whiteArrow-icon.svg";
-// import Calendar from "../../../components/calender/Calender";
-// import DoctorTable from "../../../components/Table/DoctorTable";
 
 const Home = () => {
   return (
