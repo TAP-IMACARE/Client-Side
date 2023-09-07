f63b20f3a66b9bc9c733fd7b2cc6c83d25e26f24
CALEB <owatahcaleb@gmail.com>
2023-09-07 23:58:31 +0100

    completed appointment component dashboard page

--- src/dashboards/appointment/Appointment.jsx
+++ src/dashboards/appointment/Appointment.jsx
@@ -1,253 +1,33 @@
-import scheduleIcon from "../../assets/dashboard assets/schedule-icon.svg";
-import Ubong from "../../assets/dashboard assets/Ubong.svg";
-import Chinedu from "../../assets/dashboard assets/Chinedu.svg";
-import call from "../../assets/dashboard assets/call.svg";
-import duo from "../../assets/dashboard assets/duo.svg";
-import chat from "../../assets/dashboard assets/chat.svg";
+// import scheduleIcon from "../../assets/dashboard assets/schedule-icon.svg";
+// import Ubong from "../../assets/dashboard assets/Ubong.svg";
+// import Chinedu from "../../assets/dashboard assets/Chinedu.svg";
+// import call from "../../assets/dashboard assets/call.svg";
+// import duo from "../../assets/dashboard assets/duo.svg";
+// import chat from "../../assets/dashboard assets/chat.svg";
 import "./Appointment.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import TopComponent from "../topComponent/TopComponent";
 
 const Appointment = () => {
   return (
     <div>
-      <div className="home-dashbord-top">
-        <h3>Booking Activity</h3>
-
-        <div className="dashboard-date-container">
-          <span>14 July, 2023</span>
-          <span>9:12am</span>
-        </div>
-      </div>
+      <TopComponent header={"Booking Activity"} />
 
       <div className="appointmet-wrapper">
         <div className="appointment-container">
           <ul className="appointment-nav">
-            <li className="appointment-items active">Upcoming</li>
-            <li className="appointment-items">Completed</li>
-            <li className="appointment-items">Canceled</li>
+            <NavLink to="upcoming">
+              <li className="appointment-items">Upcoming</li>
+            </NavLink>
+            <NavLink to="completed">
+              <li className="appointment-items">Completed</li>
+            </NavLink>
+            <NavLink to="canceled">
+              <li className="appointment-items">Canceled</li>
+            </NavLink>
           </ul>
 
-          <div className="appointment-cardWrapper">
-            <div className="appointment-card">
-              <h5>Dr. Ubong Etuk</h5>
-              <p>Pathologist</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts Now</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Ubong} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Ubong Etuk</h6>
-                  <p className="phone">(234) 705 435 7755</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button className="appointment-btn"> Join Meeting</button>
-              </Link>
-            </div>
-
-            {/* CARD 2 */}
-
-            <div className="appointment-card">
-              <h5>Dr. Ubong Etuk</h5>
-              <p>Pathologist</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts Now</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Ubong} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Ubong Etuk</h6>
-                  <p className="phone">(234) 705 435 7755</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button
-                  className="appointment-btn"
-                  style={{ background: "#A15E00" }}
-                >
-                  Virtual
-                </button>
-              </Link>
-            </div>
-
-            {/* CARD 3 */}
-            <div className="appointment-card">
-              <h5>Dr. Chinedu Okeke</h5>
-              <p>Surgeon</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts in 4w</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Chinedu} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Chinedu Okeke</h6>
-                  <p className="phone">(234) 705 565 7385</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button
-                  className="appointment-btn"
-                  style={{ background: "#222" }}
-                >
-                  Physical Meeting
-                </button>
-              </Link>
-            </div>
-
-            {/* CARD 4 */}
-
-            <div className="appointment-card">
-              <h5>Dr. Ubong Etuk</h5>
-              <p>Pathologist</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts in 5w</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Ubong} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Ubong Etuk</h6>
-                  <p className="phone">(234) 705 435 7755</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button
-                  className="appointment-btn"
-                  style={{ background: "#A15E00" }}
-                >
-                  Virtual
-                </button>
-              </Link>
-            </div>
-
-            {/* CARD 5 */}
-
-            <div className="appointment-card">
-              <h5>Dr. Chinedu Okeke</h5>
-              <p>Surgeon</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts in 4w</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Chinedu} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Chinedu Okeke</h6>
-                  <p className="phone">(234) 705 565 7385</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button
-                  className="appointment-btn"
-                  style={{ background: "#222" }}
-                >
-                  Physical Meeting
-                </button>
-              </Link>
-            </div>
-
-            {/* CARD 6 */}
-
-            <div className="appointment-card">
-              <h5>Dr. Ubong Etuk</h5>
-              <p>Pathologist</p>
-
-              <div className="appointment-schedule">
-                <p>10:00 am - 10:30 am</p>
-                <div className="date">
-                  <img src={scheduleIcon} alt="icon" />
-                  <span>Starts in 6w</span>
-                </div>
-              </div>
-
-              <div className="scheduled-doc-profile">
-                <img src={Ubong} className="doc-avatar" alt="avatar" />
-                <div className="doc-contact">
-                  <h6>Dr. Ubong Etuk</h6>
-                  <p className="phone">(234) 705 435 7755</p>
-                </div>
-
-                <div className="contact-doc">
-                  <img src={call} alt="icon" />
-                  <img src={duo} alt="icon" />
-                  <img src={chat} alt="icon" />
-                </div>
-              </div>
-
-              <Link to="#">
-                <button
-                  className="appointment-btn"
-                  style={{ background: "#A15E00" }}
-                >
-                  Virtual
-                </button>
-              </Link>
-            </div>
-          </div>
+          <Outlet />
         </div>
       </div>
     </div>
---
+++ src/dashboards/appointment/Completed.jsx
@@ -0,0 +1,246 @@
+import scheduleIcon from "../../assets/dashboard assets/schedule-icon.svg";
+import Ubong from "../../assets/dashboard assets/Ubong.svg";
+import Chinedu from "../../assets/dashboard assets/Chinedu.svg";
+import call from "../../assets/dashboard assets/call.svg";
+import duo from "../../assets/dashboard assets/duo.svg";
+import chat from "../../assets/dashboard assets/chat.svg";
+import Alice from "../../assets/dashboard assets/Alice.svg";
+import "./Appointment.css";
+import { Link } from "react-router-dom";
+// import TopComponent from "../topComponent/TopComponent";
+
+const Completed = () => {
+  return (
+    <div>
+      <div className="appointment-cardWrapper">
+        {/* CARD 1 */}
+        <div className="appointment-card">
+          <h5>Dr. Alice Eze</h5>
+          <p>Paramedic</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>5 days ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Alice} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Alice Eze</h6>
+              <p className="phone">(234) 705 435 7755</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+
+        {/* CARD 2 */}
+
+        <div className="appointment-card">
+          <h5>Dr. Ubong Etuk</h5>
+          <p>Pathologist</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>5 wks ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Ubong} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Ubong Etuk</h6>
+              <p className="phone">(234) 705 435 7755</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+
+        {/* CARD 3 */}
+        <div className="appointment-card">
+          <h5>Dr. Chinedu Okeke</h5>
+          <p>Surgeon</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>6 wks ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Chinedu} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Chinedu Okeke</h6>
+              <p className="phone">(234) 705 565 7385</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+
+        {/* CARD 4 */}
+
+        <div className="appointment-card">
+          <h5>Dr. Ubong Etuk</h5>
+          <p>Pathologist</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>7 wks ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Ubong} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Ubong Etuk</h6>
+              <p className="phone">(234) 705 435 7755</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+
+        {/* CARD 5 */}
+
+        <div className="appointment-card">
+          <h5>Dr. Chinedu Okeke</h5>
+          <p>Surgeon</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>2 mos ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Chinedu} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Chinedu Okeke</h6>
+              <p className="phone">(234) 705 565 7385</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+
+        {/* CARD 6 */}
+
+        <div className="appointment-card">
+          <h5>Dr. Ubong Etuk</h5>
+          <p>Pathologist</p>
+
+          <div className="appointment-schedule">
+            <p>10:00 am - 10:30 am</p>
+            <div className="date">
+              <img src={scheduleIcon} alt="icon" />
+              <span>3 mos ago</span>
+            </div>
+          </div>
+
+          <div className="scheduled-doc-profile">
+            <img src={Ubong} className="doc-avatar" alt="avatar" />
+            <div className="doc-contact">
+              <h6>Dr. Ubong Etuk</h6>
+              <p className="phone">(234) 705 435 7755</p>
+            </div>
+
+            <div className="contact-doc">
+              <img src={call} alt="icon" />
+              <img src={duo} alt="icon" />
+              <img src={chat} alt="icon" />
+            </div>
+          </div>
+
+          <Link to="#">
+            <button
+              className="appointment-btn"
+              style={{ background: "#D1D1D1" }}
+            >
+              Attended
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Completed;
