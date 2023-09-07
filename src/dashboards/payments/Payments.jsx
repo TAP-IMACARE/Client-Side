99dfc78fb799a1ee7c1db724d74ae64198b3782d
CALEB <owatahcaleb@gmail.com>
2023-09-07 23:59:23 +0100

    Update Payments.jsx

--- src/dashboards/payments/Payments.jsx
+++ src/dashboards/payments/Payments.jsx
@@ -1,7 +1,13 @@
+import TopComponent from "../topComponent/TopComponent";
 import "./Payments.css";
 
 const Payments = () => {
-  return <div>Payments</div>;
+  return (
+    <div>
+      <TopComponent header={"Payment Activity"} />
+      Payments
+    </div>
+  );
 };
 
 export default Payments;
