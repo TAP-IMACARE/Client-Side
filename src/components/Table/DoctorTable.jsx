3bb1dcc6253952248a61104c657000843225ab03
CALEB <owatahcaleb@gmail.com>
2023-09-08 00:00:57 +0100

    added Doctor table component

--- src/components/Table/DoctorTable.jsx
+++ src/components/Table/DoctorTable.jsx
@@ -1,94 +1,99 @@
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  Paper,
-} from "@mui/material";
+// import {
+//   TableContainer,
+//   Table,
+//   TableHead,
+//   TableBody,
+//   TableRow,
+//   TableCell,
+//   Paper,
+// } from "@mui/material";
 import img1 from "../../assets/dashboard assets/Ubong.svg";
 import img2 from "../../assets/dashboard assets/Chinedu.svg";
+import DataTable from "react-data-table-component";
 
 const DoctorTable = () => {
-  const tableData = [
+  const columns = [
     {
-      profile: "Dr Ubong Etuk",
-      avatar: img1,
-      service: "Pathologist",
-      date: "July 19, 2023",
-      time: "10:00am",
+      name: "Profile",
+      selector: (row) => row.profile,
     },
     {
-      profile: "Dr Chinedu Okeke",
-      avatar: img2,
-      service: "Surgeon",
-      date: "July 20, 2023",
-      time: "12:00pm",
+      name: "Service",
+      selector: (row) => row.service,
     },
     {
-      profile: "Dr Ubong Etuk",
-      avatar: img1,
-      date: "July 21, 2023",
-      time: "10:00am",
+      name: "Date",
+      selector: (row) => row.date,
     },
     {
-      profile: "Dr Chinedu Okeke",
-      avatar: img2,
-      service: "Surgeon",
-      date: "July 22, 2023",
-      time: "01:00pm",
+      name: "Time",
+      selector: (row) => row.time,
     },
   ];
+
+  const data = [
+    {
+      id: 1,
+      profile: (
+        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <img src={img1} alt="avatar" width={35} height={35} />
+          <span>Dr. Ubong Etuk</span>
+        </div>
+      ),
+      service: "Pathologist",
+      date: "July 19, 2023",
+      time: "10:00 am",
+    },
+    {
+      id: 2,
+      profile: (
+        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <img src={img2} alt="avatar" width={35} height={35} />
+          <span>Dr. Chinedu Okeke</span>
+        </div>
+      ),
+      service: "Surgeon",
+      date: "July 20, 2023",
+      time: "12:00 pm",
+    },
+    {
+      id: 3,
+      profile: (
+        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <img src={img1} alt="avatar" width={35} height={35} />
+          <span>Dr. Ubong Etuk</span>
+        </div>
+      ),
+      service: "Pathologist",
+      date: "July 21, 2023",
+      time: "10:30 am",
+    },
+
+    {
+      id: 4,
+      profile: (
+        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <img src={img2} alt="avatar" width={35} height={35} />
+          <span>Dr. Chinedu Okeke</span>
+        </div>
+      ),
+      service: "Surgeon",
+      date: "July 31, 2023",
+      time: "10:00 am",
+    },
+  ];
+
+  const customStyles = {
+    rows: {
+      style: {
+        // marginLeft: "-20px",
+      },
+    },
+  };
   return (
-    // <TableContainer component={Paper}>
-    //   <Table arial-label="table" border></Table>
-    //   <TableHead>
-    //     <TableRow>
-    //       <TableCell>Profile</TableCell>
-    //       <TableCell>Service</TableCell>
-    //       <TableCell>Date</TableCell>
-    //       <TableCell>Time</TableCell>
-    //     </TableRow>
-    //   </TableHead>
-    //   <TableBody>
-    //     {tableData.map((row, id) => (
-    //       <TableRow
-    //         key={id}
-    //         sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-    //       >
-    //         <TableCell
-    //           sx={{
-    //             justifyContent: "center",
-    //             alignItems: "center",
-    //             width: "200px",
-    //           }}
-    //         >
-    //           <img src={row.avatar} alt="picture" />
-    //           {row.profile}
-    //         </TableCell>
-    //         <TableCell>{row.service}</TableCell>
-    //         <TableCell>{row.date}</TableCell>
-    //         <TableCell>{row.time}</TableCell>
-    //       </TableRow>
-    //     ))}
-    //   </TableBody>
-    // </TableContainer>
-    <table>
-      <thead>
-        <tr>
-          <th>Profile</th>
-          <th>Service</th>
-          <th>Date</th>
-          <th>Time</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <DataTable columns={columns} data={data} customStyles={customStyles} />
+    </>
   );
 };
 
