7083137bd6976c1d9f1e5fbbc4aa87796834f5ee
CALEB <owatahcaleb@gmail.com>
2023-10-16 15:43:42 +0100

    patient communication mobile componnent

--- src/components/PatientComm/PatientCommunication.jsx
+++ src/components/PatientComm/PatientCommunication.jsx
@@ -5,16 +5,16 @@ const PatientCommunication = () => {
   return (
     <div className="PatientCommunication-container">
       <div className="PatientCommunication-left">
-        <div className="PatientCommunication-left-container">
-          <h3>
-            Patient Engagement and <br></br> Communication
-          </h3>
-          <p>
-            Reduce wait times, foster patient engagement through messaging
-            systems, and personalized communication, enhancing patient
-            satisfaction and adherence to treatment plans.
-          </p>
-        </div>
+        {/* <div className="PatientCommunication-left-container"> */}
+        <h3>
+          Patient Engagement and <br></br> Communication
+        </h3>
+        <p>
+          Reduce wait times, foster patient engagement through messaging
+          systems, and personalized communication, enhancing patient
+          satisfaction and adherence to treatment plans.
+        </p>
+        {/* </div> */}
       </div>
       <div className="PatientCommunication-right">
         <img src={patientComm} alt="image" />
