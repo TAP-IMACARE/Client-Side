8f66315ace79e07d2b4edfc1efefba214bf61974
CALEB <owatahcaleb@gmail.com>
2023-10-29 15:27:11 +0100

    Update PrescriptionTable.jsx

--- src/components/Tables/PrescriptionTable.jsx
+++ src/components/Tables/PrescriptionTable.jsx
@@ -1,136 +1,167 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+
 import img1 from "../../assets/dashboard assets/Ubong.svg";
 import img2 from "../../assets/dashboard assets/Chinedu.svg";
 import Alice from "../../assets/dashboard assets/Alice.svg";
 import DataTable from "react-data-table-component";
 
+function createData(name, item1, item2, item3, item4, item5, item6) {
+  return { name, item1, item2, item3, item4, item5, item6 };
+}
+
+const rows = [
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src={img1} width={28} height={30} />
+      <span>Dr. Ubong Etuk</span>
+    </div>,
+    "Jul 14, 2023",
+    "Propofol",
+    "50 mg",
+    "1 tab",
+    "Once-a-day (1 week)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src={img1} alt="avatar" width={28} height={30} />
+      <span>Dr. Ubong Etuk</span>
+    </div>,
+    "May 26, 2023",
+    "Sevoflurane",
+    "25 mg",
+    "1 tab",
+    "Before dinner (3 months)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      <img src={img2} alt="avatar" width={28} height={30} />
+      <span>Dr. Chinedu</span>
+    </div>,
+    "May 19, 2023",
+    "Sevoflurane",
+    "25 mg",
+    "1 tab",
+    "After lunch (1week)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      <img src={img2} alt="avatar" width={28} height={30} />
+      <span>Dr. Chinedu</span>
+    </div>,
+    "May 19, 2023",
+    "Fentanyl",
+    "25 mg",
+    "1 tab",
+    "2 times-a-day",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      <img src={Alice} alt="avatar" width={28} height={30} />
+      <span>Dr. Alice E</span>
+    </div>,
+    "May 19, 2023",
+    "Aspirin",
+    "100 mg",
+    "1 tab",
+    "Once-a-day (3 weeks)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+      <img src={Alice} alt="avatar" width={28} height={30} />
+      <span>Dr. Alice E</span>
+    </div>,
+    "May 19, 2023",
+    "Aspirin",
+    "100 mg",
+    "1 tab",
+    "Once-a-day (3 weeks)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src={img1} width={28} height={30} />
+      <span>Dr. Ubong Etuk</span>
+    </div>,
+    "Jul 14, 2023",
+    "Propofol",
+    "50 mg",
+    "1 tab",
+    "Once-a-day (1 week)",
+    <input type="checkbox" />
+  ),
+  createData(
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src={img1} width={28} height={30} />
+      <span>Dr. Ubong Etuk</span>
+    </div>,
+    "Jul 14, 2023",
+    "Propofol",
+    "50 mg",
+    "1 tab",
+    "Once-a-day (1 week)",
+    <input type="checkbox" />
+  ),
+];
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
 const PrescriptionTable = () => {
-  const columns = [
-    {
-      name: "Doctor",
-      selector: (row) => row.doctor,
-    },
-    {
-      name: "Date",
-      selector: (row) => row.date,
-    },
-    {
-      name: "Medicine",
-      selector: (row) => row.medicine,
-    },
-    {
-      name: "Strength",
-      selector: (row) => row.strength,
-    },
-    {
-      name: "Amount",
-      selector: (row) => row.amount,
-    },
-    {
-      name: "Time",
-      selector: (row) => row.time,
-    },
-    {
-      name: "Check",
-      selector: (row) => row.check,
-    },
-  ];
-
-  const data = [
-    {
-      id: 1,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={img1} alt="avatar" width={28} height={30} />
-          <span>Dr. Ubong Etuk</span>
-        </div>
-      ),
-      date: "Jul 14, 2023",
-      medicine: "Propofol",
-      strength: "50 mg",
-      amount: "1 tab",
-      time: "Once-a-day (1 week)",
-      check: <input type="checkbox" />,
-    },
-    {
-      id: 2,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={img1} alt="avatar" width={28} height={30} />
-          <span>Dr. Ubong Etuk</span>
-        </div>
-      ),
-      date: "May 26, 2023",
-      medicine: "Sevoflurane",
-      strength: "25 mg",
-      amount: "1 tab",
-      time: "Before dinner (3 months)",
-      check: <input type="checkbox" />,
-    },
-    {
-      id: 3,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={img2} alt="avatar" width={28} height={30} />
-          <span>Dr. Chinedu</span>
-        </div>
-      ),
-      date: "May 19, 2023",
-      medicine: "Sevoflurane",
-      strength: "25 mg",
-      amount: "1 tab",
-      time: "After lunch (1week)",
-      check: <input type="checkbox" />,
-    },
-    {
-      id: 4,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={img2} alt="avatar" width={28} height={30} />
-          <span>Dr. Chinedu</span>
-        </div>
-      ),
-      date: "May 19, 2023",
-      medicine: "Fentanyl",
-      strength: "25 mg",
-      amount: "1 tab",
-      time: "2 times-a-day",
-      check: <input type="checkbox" />,
-    },
-    {
-      id: 5,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={Alice} alt="avatar" width={28} height={30} />
-          <span>Dr. Alice E</span>
-        </div>
-      ),
-      date: "May 19, 2023",
-      medicine: "Aspirin",
-      strength: "100 mg",
-      amount: "1 tab",
-      time: "Once-a-day (3 weeks)",
-      check: <input type="checkbox" />,
-    },
-    {
-      id: 6,
-      doctor: (
-        <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
-          <img src={Alice} alt="avatar" width={28} height={30} />
-          <span>Dr. Alice E</span>
-        </div>
-      ),
-      date: "May 19, 2023",
-      medicine: "Aspirin",
-      strength: "100 mg",
-      amount: "1 tab",
-      time: "Once-a-day (3 weeks)",
-      check: <input type="checkbox" />,
-    },
-  ];
-
   return (
     <>
-      <DataTable columns={columns} data={data} />
+      {/* <DataTable columns={columns} data={data} /> */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Doctor</TableCell>
+              <TableCell>Date </TableCell>
+              <TableCell>Medicine</TableCell>
+              <TableCell>Strength</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Time</TableCell>
+              <TableCell>Check</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, i) => (
+              <StyledTableRow
+                key={i}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell>{row.item1}</TableCell>
+                <TableCell>{row.item2}</TableCell>
+                <TableCell>{row.item3}</TableCell>
+                <TableCell>{row.item4}</TableCell>
+                <TableCell>{row.item5}</TableCell>
+                <TableCell>{row.item6}</TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
