83b81e0dd5dfd8e1795c5e0479a12ae914819242
CALEB <owatahcaleb@gmail.com>
2023-10-30 19:23:10 +0100

    Update Settings.jsx

--- src/dashboards/settings/Settings.jsx
+++ src/dashboards/settings/Settings.jsx
@@ -1,11 +1,31 @@
 import TopComponent from "../topComponent/TopComponent";
 import "./Settings.css";
 
+import { Link, NavLink, Outlet } from "react-router-dom";
+
 const Settings = () => {
   return (
     <div>
       <TopComponent header={"Settings"} />
-      Settings
+      <div className="settings-container">
+        <div className="settings-leftContainer">
+          <Outlet />
+        </div>
+
+        <div className="settings-rightComponent">
+          <ul className="settings-nav" id="settings-nav">
+            <NavLink to={"info"}>
+              <li>Personal Information</li>
+            </NavLink>
+            <NavLink to={"files"}>
+              <li>Attached Files</li>
+            </NavLink>
+            <Link to="files">
+              <li>Change Password</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
