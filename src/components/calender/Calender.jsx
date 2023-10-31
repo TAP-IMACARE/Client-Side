638c5dfbd9a6a993bde8e7942e1e4e358782f027
CALEB <owatahcaleb@gmail.com>
2023-10-31 22:07:45 +0100

    Update Calender.jsx

--- src/components/calender/Calender.jsx
+++ src/components/calender/Calender.jsx
@@ -1,18 +1,31 @@
+/* eslint-disable react/prop-types */
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
 import "./Calender.css";
 
-export default function Calendar() {
+export default function Calendar({ onChange, value }) {
+  // console.log(date);
   return (
     <div className="calender">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateCalendar
+          disablePast
           sx={{
-            maxHeight: "280px",
+            maxHeight: "290px",
             // paddingBottom: "80px",
             // borderBottom: "0.5px solid #D1D1D1",
           }}
+          value={value}
+          onChange={onChange}
+          slotProps={{
+            toolbar: {
+              toolbarFormat: "DD MM YYYY",
+              toolbarPlaceholder: "??",
+            },
+          }}
+          selected={true}
         />
       </LocalizationProvider>
     </div>
