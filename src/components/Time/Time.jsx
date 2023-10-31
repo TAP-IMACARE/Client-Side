d7f8543eb7639d846871bd2167771516e1c4d321
CALEB <owatahcaleb@gmail.com>
2023-10-31 22:04:40 +0100

    Create Time.jsx

---
+++ src/components/Time/Time.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { TimePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import "./Time.css";
+
+const Time = () => {
+  const [value, setValue] = useState(dayjs("2022-04-17T15:30"));
+  return (
+    <>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <TimePicker
+          value={value}
+          onChange={(newValue) => setValue(newValue)}
+          selected={true}
+        />
+      </LocalizationProvider>
+
+      <div>{value.format("hh:mm a")}</div>
+    </>
+  );
+};
+
+export default Time;
