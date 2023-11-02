a6703f16ec9d77f013ead19ce1a5b4463ccad1d5
CALEB <owatahcaleb@gmail.com>
2023-11-02 15:06:52 +0100

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/header/Header";
-import Hero from "../components/Hero/Hero";
+import Hero from "../components/hero/Hero";
 import PatientInfoContent from "../components/patientInfoContent/PatientInfoContent";
 import WhyUs from "../components/whyUs/WhyUs";
 import Footer from "../components/footer/Footer";
