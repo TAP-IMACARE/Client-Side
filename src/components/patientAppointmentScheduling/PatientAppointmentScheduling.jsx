29311fa95d1f2a85052ed8a985ae2ab8268db7dd
CALEB <owatahcaleb@gmail.com>
2023-11-04 21:10:19 +0100

    Update PatientAppointmentScheduling.jsx

--- src/components/patientAppointmentScheduling/PatientAppointmentScheduling.jsx
+++ src/components/patientAppointmentScheduling/PatientAppointmentScheduling.jsx
@@ -3,18 +3,20 @@ import img from "../../assets/landingPageAssets/setAppointment.png";
 
 const PatientAppointmentScheduling = () => {
   return (
-    <div className="PatientAppointmentScheduling-container">
-      <div className="PatientAppointmentScheduling-left">
-        <img src={img} alt="image" />
-      </div>
-      <div className="PatientAppointmentScheduling-right">
-        <div className="PatientAppointmentScheduling-right-container">
-          <h3>Appointment Scheduling</h3>
-          <p>
-            Streamline the appointment booking process for patients, doctors,
-            and other healthcare professionals, ensuring optimal resource
-            allocation and reduced wait times.
-          </p>
+    <div className="patientAppointmentSchedulling-wrapper">
+      <div className="PatientAppointmentScheduling-container">
+        <div className="PatientAppointmentScheduling-left">
+          <img src={img} alt="image" />
+        </div>
+        <div className="PatientAppointmentScheduling-right">
+          <div className="PatientAppointmentScheduling-right-container">
+            <h3>Appointment Scheduling</h3>
+            <p>
+              Streamline the appointment booking process for patients, doctors,
+              and other healthcare professionals, ensuring optimal resource
+              allocation and reduced wait times.
+            </p>
+          </div>
         </div>
       </div>
     </div>
