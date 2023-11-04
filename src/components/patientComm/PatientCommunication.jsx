f6559c110ad870cf3722e5ca40e477a56d627a66
CALEB <owatahcaleb@gmail.com>
2023-11-04 21:10:30 +0100

    Update PatientCommunication.jsx

--- src/components/patientComm/PatientCommunication.jsx
+++ src/components/patientComm/PatientCommunication.jsx
@@ -3,21 +3,23 @@ import patientComm from "../../assets/landingPageAssets/patientComm.png";
 
 const PatientCommunication = () => {
   return (
-    <div className="PatientCommunication-container">
-      <div className="PatientCommunication-left">
-        {/* <div className="PatientCommunication-left-container"> */}
-        <h3>
-          Patient Engagement and <br></br> Communication
-        </h3>
-        <p>
-          Reduce wait times, foster patient engagement through messaging
-          systems, and personalized communication, enhancing patient
-          satisfaction and adherence to treatment plans.
-        </p>
-        {/* </div> */}
-      </div>
-      <div className="PatientCommunication-right">
-        <img src={patientComm} alt="image" />
+    <div className="patientCommunicationWrapper">
+      <div className="PatientCommunication-container">
+        <div className="PatientCommunication-left">
+          {/* <div className="PatientCommunication-left-container"> */}
+          <h3>
+            Patient Engagement and <br></br> Communication
+          </h3>
+          <p>
+            Reduce wait times, foster patient engagement through messaging
+            systems, and personalized communication, enhancing patient
+            satisfaction and adherence to treatment plans.
+          </p>
+          {/* </div> */}
+        </div>
+        <div className="PatientCommunication-right">
+          <img src={patientComm} alt="image" />
+        </div>
       </div>
     </div>
   );
