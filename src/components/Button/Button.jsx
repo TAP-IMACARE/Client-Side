99ceb16fc11b15248a626ba87f76c371813fd908
CALEB <owatahcaleb@gmail.com>
2023-11-17 01:42:13 +0100

    Custom Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Button.css";
+
+// eslint-disable-next-line react/prop-types
+const Button = ({ name }) => {
+  return <button className="custom-BTN">{name}</button>;
+};
+
+export default Button;
