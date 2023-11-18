bdbaec91332d3697d0e0dc697c14927fa17cc1df
CALEB <owatahcaleb@gmail.com>
2023-11-18 21:48:22 +0100

    created profile component

---
+++ src/dashboards/profile/Profile.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./Profile.css";
+import TopComponent from "../topComponent/TopComponent";
+
+const Profile = () => {
+  return (
+    <>
+      <TopComponent header={"Profile Information"} />
+    </>
+  );
+};
+
+export default Profile;
