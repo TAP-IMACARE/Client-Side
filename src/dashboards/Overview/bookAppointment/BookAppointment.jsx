aa11a9c3b3680efafe666035b0783ddd2718a4ad
CALEB <owatahcaleb@gmail.com>
2023-11-23 18:29:35 +0100

    Update BookAppointment.jsx

--- src/dashboards/Overview/bookAppointment/BookAppointment.jsx
+++ src/dashboards/Overview/bookAppointment/BookAppointment.jsx
@@ -10,6 +10,26 @@ import "./BookAppointment.css";
 import { Link, useNavigate } from "react-router-dom";
 
 const BookAppointment = () => {
+  const doctorsData = [
+    {
+      avatar: img1,
+      name: "Dr. Ubong Etuk",
+      portfolio: "Pathology Specialist",
+      info: "A sought-after pathologist, known for his exceptional diagnosti abilities, and his unwavering dedication to patient care.",
+    },
+    {
+      avatar: img2,
+      name: "Dr. Alice Eze",
+      portfolio: "Paramedic Specialist",
+      info: "Extensive knowledge in advanced life support techniques, trauma management, pre-hospital care, emergency medicine, and disaster management.",
+    },
+    {
+      avatar: img3,
+      name: "Dr. Chinedu Okeke",
+      portfolio: "Surgical Specialist",
+      info: "A highly skilled and accomplished surgical specialist with expertise in Gynecology. Dedicated in making impact on the field of surgery and improving lives.",
+    },
+  ];
   const navigate = useNavigate();
   const handleNavigate = () => {
     navigate("set-appointment");
@@ -21,36 +41,40 @@ const BookAppointment = () => {
 
       <div className="bookAppointment-container">
         {/* Card 1 */}
-        <div className="bookAppointment">
-          <img src={img1} alt="avatar" className="doc-avatar" />
+        {doctorsData.map((data, i) => (
+          <div key={i}>
+            <div className="bookAppointment">
+              <img src={data.avatar} alt="avatar" className="doc-avatar" />
 
-          <div className="doctor-portfolio">
-            <h3>Dr. Ubong Etuk</h3>
-            <h5>Pathology Specialist</h5>
+              <div className="doctor-portfolio">
+                <h3>{data.name}</h3>
+                <h5>{data.portfolio}</h5>
 
-            <p>
-              A sought-after pathologist, known for his exceptional diagnostic
-              abilities, and his unwavering dedication to patient care.
-            </p>
+                <p>{data.info}</p>
 
-            <div className="bottomContent ">
-              <div className="doc-rating">
-                <img src={star} alt="avatar" />
-                <p style={{ marginRight: "15px", fontSize: "13px" }}>4.5</p>
-                <p style={{ fontSize: "13px" }}>5 years of experience</p>
-              </div>
+                <div className="bottomContent ">
+                  <div className="doc-rating">
+                    <img src={star} alt="avatar" />
+                    <p style={{ marginRight: "15px", fontSize: "13px" }}>4.5</p>
+                    <p style={{ fontSize: "13px" }}>5 years of experience</p>
+                  </div>
 
-              <div className="appointment-btn">
-                <Link to={"/dashboard/set-appointment"} id="appointment-btn">
-                  Book Appointment
-                </Link>
+                  <div className="appointment-btn">
+                    <Link
+                      to={"/dashboard/set-appointment"}
+                      id="appointment-btn"
+                    >
+                      Book Appointment
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
 
         {/* CARD 2 */}
-        <div className="bookAppointment">
+        {/* <div className="bookAppointment">
           <img src={img2} alt="avatar" className="doc-avatar" />
 
           <div className="doctor-portfolio">
@@ -77,10 +101,10 @@ const BookAppointment = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* CARD 3 */}
-        <div className="bookAppointment">
+        {/* <div className="bookAppointment">
           <img src={img3} alt="avatar" className="doc-avatar" />
 
           <div className="doctor-portfolio">
@@ -107,7 +131,7 @@ const BookAppointment = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
